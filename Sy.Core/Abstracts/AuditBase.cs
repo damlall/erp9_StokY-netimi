f64088559df423c05de789136c3248d2d2c604a1
damla altun <damla.alltun@gmail.com>
2019-09-03 10:49:17 +0300

    .

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class AuditBase //Bu nesneden nesne oluşsun istemiyorum ama kaltım için kullanmak istiyorum
+    {
+
+        [StringLength(50)]
+        public int CreatedUser { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        [StringLength(50)]
+        public int UpdateUser { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+
+    }
+}
---
+++ Sy.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
+    {
+        [Key] // Yazmasakta olur ıd yi algılıyor zaten
+        public TKey Id { get; set; }
+    }
+}
