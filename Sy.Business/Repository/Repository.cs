f39577655a8c7fb63c11eefbd3142e379db93da2
Sabah <Sabah@WISSEN>
2019-09-03 15:06:52 +0300

    repo okay

---
+++ Sy.Business/Repository/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Sy.Core.Abstracts;
+using Sy.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Business.Repository
+{
+    public class Repository<TEntity, Tkey> : IRepository<TEntity, Tkey> where TEntity : class,IEntity<Tkey>
+    {
+        private readonly StockDbContext _context;
+        private readonly DbSet<TEntity> _table;
+
+        public Repository()
+        {
+            _context = new StockDbContext();
+            _table = _context.Set<TEntity>();
+        }
+       
+        public int Delete(TEntity entity)
+        {
+            _table.Remove(entity);
+            return this.Save();
+                }
+
+        public TEntity GetById(Tkey id)
+        {
+            return _table.Find(id);
+        }
+
+        public Tkey Insert(TEntity entity)
+        {
+            _table.Add(entity);
+            this.Save();
+            return entity.Id;
+        }
+
+        public IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            return predicate == null ? _table : _table.Where(predicate);
+        }
+
+        public int Save()
+        {
+            return _context.SaveChanges();
+        }
+
+        public int Update(TEntity entity)
+        {
+            _table.AddOrUpdate(entity);
+            return this.Save();
+        }
+    }
+}
---
+++ Sy.Core/Abstracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public interface IRepository<TEntity,Tkey> 
+        where TEntity : IEntity<Tkey> // iki tiple çalışcak entitynn tipi bide anahtarının tipi tentitye bağlı şeyler ıentıtyden gelsin tkey le bağladık
+    {
+        TEntity GetById(Tkey id);
+        Tkey Insert(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        int Save();
+        IQueryable<TEntity> Query(Expression<Func<TEntity,bool>> predicate = null);
+
+    }
+}
