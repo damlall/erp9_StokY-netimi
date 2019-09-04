28e695cc8a82f24f1fde9bc85bcd037d06d75e3d
Sabah <Sabah@WISSEN>
2019-09-04 10:11:57 +0300

    register

--- Sy.Core/Entities/Client.cs
+++ Sy.Core/Entities/Client.cs
@@ -1,4 +1,5 @@
 ﻿using Sy.Core.Abstracts;
+using Sy.Core.Enums;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,7 +13,7 @@ namespace Sy.Core.Entities
     [Table("Clients")]
     public class Client : BaseEntity<int>
     {
-        [Required,StringLength(50)]
+        [Required, StringLength(50)]
         public string Name { get; set; }
         [Required, StringLength(50)]
         public string Surname { get; set; }
@@ -20,6 +21,7 @@ namespace Sy.Core.Entities
         public string Password { get; set; }
         [Required, StringLength(50)]
         public string Email { get; set; }
+        public ApplicationRole ApplicationRole {get; set;}
 
     }
 }
--- Sy.Forms/Auth/RegisterForm.cs
+++ Sy.Forms/Auth/RegisterForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Sy.Business.Repository;
+using Sy.Core.Entities;
+using Sy.Core.Abstracts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,18 +10,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sy.Core.ViewModels;
+using Sy.Core.Enums;
 
 namespace Sy.Forms.Auth
 {
     public partial class RegisterForm : Form
     {
+        private readonly Core.Abstracts.IRepository<Client, int> _clientRepository;
         public RegisterForm()
         {
             InitializeComponent();
+            _clientRepository = new Repository<Client, int>();
         }
 
         private void btnKaydol_Click(object sender, EventArgs e)
         {
+            var model = new RegisterViewModel()
+            {
+                Email = txtEmail.Text,
+                Name = txtAd.Text,
+                Surname = txtSoyad.Text,
+                Password = txtSıfre.Text
+
+            };
+            // email var mı kontrol edelim...
+            var kontrol = _clientRepository.Query(x => x.Email == model.Email).Any();
+
+            if (kontrol)
+            {
+                MessageBox.Show("Bu email adresi kullanılmaktadır!");
+                return; // bu metodun dışınaa çıkmamızı ve aşağıdaki kodları görmeyi engeller
+            }
+
+            //benzersiz email kontrolünden sonra kişiyi kaydedelim...
+
+            var adminMi =! _clientRepository.Query().Any();
+           
+            _clientRepository.Insert(new Client()
+            {
+
+                Email = model.Email,
+                Name = model.Name,
+                Surname = model.Surname,
+                Password = model.Password,
+                ApplicationRole = adminMi ? ApplicationRole.Customer : ApplicationRole.Admin
+
+            });
+            MessageBox.Show($"Kayıt işleminiz başarılıdır.\n {model.Name} {model.Surname}");
+
+            this.Close(); //bulunduğu classı işaret eder 
+            //base kalıtım aldığımız classı işaret eder içindeki consructor ile birlikte
+
+
 
         }
     }
